663a8ebe56106a1023a92b03029d5b5544cb261a
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-11-06 13:16:17 +0100

    initial commit

---
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/ClinicalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClinicalSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalSystemApplication.class, args);
+	}
+
+}
---
+++ ClinicalSystem/src/test/java/com/example/ClinicalSystem/ClinicalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClinicalSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
