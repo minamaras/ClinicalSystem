5223ffd2a3f57e6b39700d86d9429f563774cf8b
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-11-06 20:57:39 +0100

    Added Doctor model

--- ClinicalSystem/src/main/java/model/Doctor.java
+++ ClinicalSystem/src/main/java/model/Doctor.java
@@ -1,5 +1,174 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Doctor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "firstName", nullable = false)
+	private String firstName;
+	
+	@Column(name = "lastName", nullable = false)
+	private String lastName;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "specialization", nullable = false)
+	private String specialization;
+	
+	@Column(name = "rating", nullable = false)
+	private String rating;
+	
+	@Column(name = "username", nullable = false)
+	private String username;
+	
+	@OneToMany(mappedBy = "doctor", fetch = FetchType.LAZY )
+	private Set<Patient> patients = new HashSet<Patient>();
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Clinic clinic;
+
+	public Doctor() {
+		super();
+	}
+
+	public Doctor(String firstName, String lastName, String password, String email, String specialization,
+			String username, Set<Patient> patients, Clinic clinic) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.specialization = specialization;
+		this.username = username;
+		this.patients = patients;
+		this.clinic = clinic;
+	}
+
+	public Doctor(Long id, String firstName, String lastName, String password, String email, String specialization,
+			String rating, String username, Set<Patient> patients, Clinic clinic) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.specialization = specialization;
+		this.rating = rating;
+		this.username = username;
+		this.patients = patients;
+		this.clinic = clinic;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Set<Patient> getPatients() {
+		return patients;
+	}
+
+	public void setPatients(Set<Patient> patients) {
+		this.patients = patients;
+	}
+
+	public Clinic getClinic() {
+		return clinic;
+	}
+
+	public void setClinic(Clinic clinic) {
+		this.clinic = clinic;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
