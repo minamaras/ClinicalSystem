9c99bc6f0cee75ca54f1a9d65057c64b6ef385df
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-11-09 20:26:50 +0100

    Added ClinicAdmin service, controller and repository

---
+++ ClinicalSystem/src/main/java/controller/ClinicAdminController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import service.ClinicAdminService;
+
+
+@RestController
+@RequestMapping(value = "api/clinicadmin")
+public class ClinicAdminController {
+
+	@Autowired
+	ClinicAdminService clinicAdminService;
+}
---
+++ ClinicalSystem/src/main/java/repository/ClinicAdminRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.ClinicAdmin;
+
+public interface ClinicAdminRepository extends JpaRepository<ClinicAdmin, Long>{
+	
+	
+
+}
