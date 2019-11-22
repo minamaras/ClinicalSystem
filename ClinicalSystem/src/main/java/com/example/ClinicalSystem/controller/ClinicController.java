b0ca2b4961494c5e5248a682c73c8aeae71bd92d
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-11-22 16:15:04 +0100

    Changed value for addClinic method in ClinicController

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/ClinicController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/ClinicController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +17,9 @@ import com.example.ClinicalSystem.DTO.ClinicDTO;
 import com.example.ClinicalSystem.model.Clinic;
 import com.example.ClinicalSystem.service.ClinicService;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
+@RequestMapping(value = "api/clinics")
 public class ClinicController {
 	
 	@Autowired 
@@ -25,7 +28,7 @@ public class ClinicController {
 	@Autowired
 	ModelMapper modelMapper;
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/addclinic")
+	@RequestMapping(method = RequestMethod.POST, value = "/addClinic")
 	public ResponseEntity<ClinicDTO> addClinic(@RequestBody ClinicDTO clinicDTO) {
 
 		clinicService.addClinic(clinicDTO);
