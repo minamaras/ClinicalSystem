0e35c0926fb80cb79258ed50e38d3e141f7a3f3f
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2019-11-22 12:26:44 +0100

    refactored UserService

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/UserService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/UserService.java
@@ -1,6 +1,10 @@
 package com.example.ClinicalSystem.service;
 
+import com.example.ClinicalSystem.DTO.UserDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.ClinicalSystem.model.User;
@@ -12,11 +16,36 @@ public class UserService {
 		
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	
-	public User findByEmail(String email) {
-		
-		return userRepository.findByEmail(email);
+	public ResponseEntity<UserDTO> loginUser(UserDTO user) {
+
+		User u = modelMapper.map(user,User.class);
+		boolean pronadjen = false;
+
+		if(userRepository.findByEmail(u.getEmail()) == null){
+
+			pronadjen = false;
+		}
+
+		else if (userRepository.findByEmail(u.getEmail()) ==  userRepository.findByPassword(u.getPassword())){
+
+			pronadjen = true;
+
+		}
+
+		if(pronadjen){
+
+			return  new ResponseEntity<UserDTO>(HttpStatus.OK);
+		}else {
+
+			return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+
 	}
 	
 
