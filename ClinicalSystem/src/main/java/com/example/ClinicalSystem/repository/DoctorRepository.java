973242fe38cdfef2353ebe88a0f82b27cae1c7e7
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-12-02 12:44:47 +0100

    Implemented method for deletign and finding doctors by email

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/DoctorRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/DoctorRepository.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.example.ClinicalSystem.model.Doctor;
 
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 	
 	Doctor save(Doctor doctor);
 
+	Doctor findByEmail(String email);
+
+	void deleteByEmail(String email);
 }
