0f0728090cd406823d44f50704e7b16e95d256df
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-12-03 16:26:42 +0100

    Added save method

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/OperationRoomRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/OperationRoomRepository.java
@@ -1,10 +1,9 @@
 package com.example.ClinicalSystem.repository;
 
-import com.example.ClinicalSystem.model.ClinicalCentreAdmin;
 import com.example.ClinicalSystem.model.OR;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface OperationRoomRepository extends JpaRepository<OR, Long>  {
+public interface OperationRoomRepository extends JpaRepository<OR, Long> {
+
+    OR save(OR or);
 }
