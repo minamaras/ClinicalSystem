12564b85ad53a2932c4f2b6abf2150bb66c76f8d
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2019-12-03 14:53:39 +0100

    Added authority

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/NurseService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/NurseService.java
@@ -1,8 +1,10 @@
 package com.example.ClinicalSystem.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.example.ClinicalSystem.model.Authority;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,9 @@ public class NurseService {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private AuthorityService authorityService;
 	
 	public List<Nurse> findAll(){
 		return nurseRepository.findAll();
@@ -27,6 +32,12 @@ public class NurseService {
 	public Nurse save(Nurse nurse) {
 
 		nurse.setPassword(passwordEncoder.encode(nurse.getPassword()));
+		Authority authoritie = authorityService.findByname("NURSE");
+		List<Authority> authorities = new ArrayList<>();
+		authorities.add(authoritie);
+		nurse.setAuthorities(authorities);
+
+
 		return nurseRepository.save(nurse);
 	}
 	
