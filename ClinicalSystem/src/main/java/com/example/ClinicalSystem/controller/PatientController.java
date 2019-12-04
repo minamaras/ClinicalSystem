7b617606169d0b36eb3beec76459a872fe51c9f4
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-04 01:48:07 +0100

    implemented account confirmation by email

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/PatientController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/PatientController.java
@@ -5,11 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.ClinicalSystem.DTO.PatientDTO;
-import com.example.ClinicalSystem.DTO.UserDTO;
 import com.example.ClinicalSystem.model.Patient;
-import com.example.ClinicalSystem.model.User;
 import com.example.ClinicalSystem.service.PatientService;
+
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping(value = "api/patients")
@@ -18,19 +16,32 @@ public class PatientController {
 	@Autowired
 	private PatientService patientService;
 
-	/*@RequestMapping(method = RequestMethod.POST, value = "/register")
-	public ResponseEntity<?> register(@RequestBody PatientDTO patientDTO) {
+	@RequestMapping(method= RequestMethod.GET, value="/confirm-account/{verificationCode}")
+	public ResponseEntity<?> confirmUserAccount(@PathVariable("verificationCode") String verificationCode)
+	{
+		//ConfirmationToken token = confirmationTokenService.findByConfirmationToken(verificationCode);
+		Patient patient = patientService.findVerificationCode(verificationCode);
 
-			boolean registered = patientService.register(patientDTO);
+		if(patient.getVerificationCode() != null)
+		{
+			try {
+				//Patient p = patientService.findPatient(token.getPatient().getEmail());
+				patient.setActive(true);
+				patientService.savePatient(patient);
 
-			if(registered){
+				return new ResponseEntity<>("User "+ patient.getName() +" is successfully registered!", HttpStatus.OK);
 
-				return new ResponseEntity<>(HttpStatus.CREATED);
+			} catch (Exception e){
+				return new ResponseEntity<>("Account activation is unsuccessful.", HttpStatus.NOT_FOUND);
 			}
-			else {
-				return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-			}
-	}*/
+
+		}
+		else
+		{
+			return new ResponseEntity<>("The link is invalid.", HttpStatus.NOT_FOUND);
+		}
+
+	}
 
 
 }
