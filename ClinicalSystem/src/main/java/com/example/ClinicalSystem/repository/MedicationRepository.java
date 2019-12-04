9f9eaaa44475df71e5456a612c9042581ad48796
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-04 03:08:22 +0100

    created Medication Repository

---
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/MedicationRepository.java
@@ -0,0 +1,15 @@
+package com.example.ClinicalSystem.repository;
+
+import com.example.ClinicalSystem.model.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+
+    Medication save(Medication diagnosis);
+    List<Medication> findAll();
+    Medication findByName(String name);
+}
