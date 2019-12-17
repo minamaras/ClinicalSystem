29e2643a1bb2708f72a204ad7aec155ed5f60479
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2019-12-17 17:25:45 +0100

    changed relationships between predefiend clinic and doctor

--- ClinicalSystem/src/main/resources/data-postgres.sql
+++ ClinicalSystem/src/main/resources/data-postgres.sql
@@ -3,12 +3,14 @@ insert into users (id,name, lastname, email, password, role,last_password_reset_
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-4,'mina', 'maras', 'mina.maras@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'DOCTOR','2019-12-01 09:00:01');
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-3,'tamara', 'jancic', 'tamara@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'CLINICADMIN','2019-12-01 09:00:01');
 
+
 INSERT INTO clinic (id, description, name, adress,rating) values (-3, 'Klinika broj 1', 'Klinika1', 'Bulevar Oslobodjenja 22','9');
-insert into doctor (id,specialization,rating) values (-4,'zubar','10');
+insert into clinic_admin (id,clinic_id) values (-3,-3);
+insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id) values (-4,'zubar','10',-3,-3);
 insert into clinical_centre_admin (id) values (-6);
 
 insert into patient (id,active,adress,city,country,phone,socialSecurityNumber) values (-5,'true','Bulevar Oslobodjenja 11','Novi Sad','Srbija','064335512','0301997805089');
-insert into clinic_admin (id,clinic_id) values (-3,-3);
+
 insert into clinic_admin_doctors(clinic_admin_id,doctors_id) values(-3,-4);
 insert into clinic_doctors (clinic_id,doctors_id) values (-3,-4);
 
