885b17dbeac01d6f64b88f4b1454572f0a9bd17f
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2019-12-17 16:36:37 +0100

    addded method that finds doctor by id

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/DoctorService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/DoctorService.java
@@ -1,18 +1,18 @@
 package com.example.ClinicalSystem.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import com.example.ClinicalSystem.model.Authority;
+import com.example.ClinicalSystem.DTO.ClinicDTO;
+import com.example.ClinicalSystem.model.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.ClinicalSystem.DTO.DoctorDTO;
 import com.example.ClinicalSystem.DTO.UserDTO;
-import com.example.ClinicalSystem.model.Doctor;
-import com.example.ClinicalSystem.model.Role;
 import com.example.ClinicalSystem.repository.DoctorRepository;
 
 import javax.transaction.Transactional;
@@ -34,6 +34,10 @@ public class DoctorService {
 
 	@Autowired
 	private AuthorityService authorityService;
+
+	@Autowired
+	private ClinicService clinicService;
+
 
 	public List<DoctorDTO> findAll() {
 		
@@ -92,4 +96,14 @@ public class DoctorService {
 	public Doctor findOne(String email) {
 		return doctorRepository.findByEmail(email);
 	}
+	public Doctor findOneById(Long id) {
+
+		Optional<User> user = userService.findById(id);
+		User u = user.get();
+		Doctor doctor = doctorRepository.findByEmail(u.getEmail());
+
+		return doctor;
+	}
+
+
 }
