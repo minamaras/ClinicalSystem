12c84a15aec9c505d2569b8e68e28fc0cb1aca9b
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-12-17 17:41:54 +0100

    Refactored save doctor method

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/DoctorService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/DoctorService.java
@@ -1,9 +1,11 @@
 package com.example.ClinicalSystem.service;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.ClinicalSystem.model.Authority;
+import com.example.ClinicalSystem.DTO.NurseDTO;
+import com.example.ClinicalSystem.model.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,8 +13,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.ClinicalSystem.DTO.DoctorDTO;
 import com.example.ClinicalSystem.DTO.UserDTO;
-import com.example.ClinicalSystem.model.Doctor;
-import com.example.ClinicalSystem.model.Role;
 import com.example.ClinicalSystem.repository.DoctorRepository;
 
 import javax.transaction.Transactional;
@@ -50,24 +50,29 @@ public class DoctorService {
 	public Doctor updateDoctor(Doctor doctor) {
 		return doctorRepository.save(doctor);
 	}
-	
-	public Doctor saveDoctor(DoctorDTO doctorDto) {
-		
-		UserDTO userDto = modelMapper.map(doctorDto, UserDTO.class);
-		if(userService.existsInDB(userDto)) {
-			return null;
+
+	public Doctor save(DoctorDTO doctorDTO, Principal p) {
+
+		ClinicAdmin cAdmin = (ClinicAdmin) userService.findByUsername(p.getName());
+		Clinic clinic = cAdmin.getClinic();
+
+
+		Doctor doctor = modelMapper.map(doctorDTO, Doctor.class);
+
+		if(clinic != null) {
+			doctor.setClinic(clinic);
+			clinic.getDoctors().add(doctor);
 		}
-		
-		Doctor doctor = modelMapper.map(doctorDto, Doctor.class);
+
+		doctor.setPassword(passwordEncoder.encode(doctor.getPassword()));
+
 		Authority authoritie = authorityService.findByname("DOCTOR");
 		List<Authority> authorities = new ArrayList<>();
 		authorities.add(authoritie);
 		doctor.setAuthorities(authorities);
 
-		doctor.setPassword(passwordEncoder.encode(doctor.getPassword()));
-
-        return doctorRepository.save(doctor);
-    }
+		return doctorRepository.save(doctor);
+	}
 
     @Transactional
     public boolean removeDoctor(DoctorDTO doctorDto) {
