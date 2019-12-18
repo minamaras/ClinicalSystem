5eb75b43363426359d21af8fa4d6ab26728f69e0
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-18 13:00:38 +0100

    added nurse in database

--- ClinicalSystem/src/main/resources/data-postgres.sql
+++ ClinicalSystem/src/main/resources/data-postgres.sql
@@ -2,17 +2,20 @@ insert into users (id,name, lastname, email, password, role,last_password_reset_
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-5,'jelena', 'bojanic', 'jelena.bojanic97@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'PATIENT','2019-12-01 09:00:01');
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-4,'mina', 'maras', 'mina.maras@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'DOCTOR','2019-12-01 09:00:01');
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-3,'tamara', 'jancic', 'tamara@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'CLINICADMIN','2019-12-01 09:00:01');
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-8,'sestra', 'sestr', 'sestra@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'NURSE','2019-12-01 09:00:01');
 
 
 INSERT INTO clinic (id, description, name, adress,rating) values (-3, 'Klinika broj 1', 'Klinika1', 'Bulevar Oslobodjenja 22','9');
 insert into clinic_admin (id,clinic_id) values (-3,-3);
 insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id) values (-4,'zubar','10',-3,-3);
 insert into clinical_centre_admin (id) values (-6);
+insert into nurse (id) values (-8);
 
 insert into patient (id,active,adress,city,country,phone,socialSecurityNumber) values (-5,'true','Bulevar Oslobodjenja 11','Novi Sad','Srbija','064335512','0301997805089');
 
 insert into clinic_admin_doctors(clinic_admin_id,doctors_id) values(-3,-4);
 insert into clinic_doctors (clinic_id,doctors_id) values (-3,-4);
+insert into clinic_nurses (clinic_id,nurses_id) values (-3,-8);
 
 
 INSERT INTO authority (id,name) values (1,'PATIENT');
@@ -25,3 +28,4 @@ INSERT INTO user_authority (user_id,authority_id) values (-6,5);
 INSERT INTO user_authority (user_id,authority_id) values (-5,1);
 INSERT INTO user_authority (user_id,authority_id) values (-4,2);
 INSERT INTO user_authority (user_id,authority_id) values (-3,4);
+INSERT INTO user_authority (user_id,authority_id) values (-8,3);
