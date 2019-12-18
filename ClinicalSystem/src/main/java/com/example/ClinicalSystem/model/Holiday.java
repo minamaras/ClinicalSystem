3e6d155aa45acc9bc098cc4e84ef6e739721be70
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-18 13:15:45 +0100

    added dates for holiday

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/model/Holiday.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/model/Holiday.java
@@ -1,6 +1,7 @@
 package com.example.ClinicalSystem.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="holidays")
@@ -22,13 +23,22 @@ public class Holiday {
     @Column(name="reason")
     private String reason;
 
+    @Column(name="start")
+    private Date start;
+
+    @Column(name="end")
+    private Date end;
+
+
     public Holiday() {
 
     }
-    
-    public Holiday(HolidayType type, String reason){
+
+    public Holiday(HolidayType type, String reason, Date start, Date end){
         this.type = type;
         this.reason = reason;
+        this.start = start;
+        this.end = end;
     }
 
     public Long getId() {
@@ -53,5 +63,21 @@ public class Holiday {
 
     public void setReason(String reason) {
         this.reason = reason;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
     }
 }
