a1cd05f72ba6dc124110746ef995aacc1e874ce4
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-18 13:36:15 +0100

    implemented findAll method

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/HolidayService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/HolidayService.java
@@ -1,9 +1,16 @@
 package com.example.ClinicalSystem.service;
 
+import com.example.ClinicalSystem.DTO.ClinicAdminDTO;
+import com.example.ClinicalSystem.DTO.HolidayDTO;
+import com.example.ClinicalSystem.model.ClinicAdmin;
+import com.example.ClinicalSystem.model.Holiday;
 import com.example.ClinicalSystem.repository.HolidayRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class HolidayService {
@@ -13,4 +20,17 @@ public class HolidayService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    public List<HolidayDTO> findAll() {
+        List<Holiday> holidays = holidayRepository.findAll();
+
+        List<HolidayDTO> holidaysDTO = new ArrayList<>();
+        for (Holiday h : holidays) {
+            holidaysDTO.add(new HolidayDTO(h));
+        }
+
+        return holidaysDTO;
+    }
+
+    
 }
