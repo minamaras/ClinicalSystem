e0165191367ae0d2bb52765be7d95a23b963d081
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-18 14:55:32 +0100

    created method for making requests

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/HolidayController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/HolidayController.java
@@ -1,10 +1,14 @@
 package com.example.ClinicalSystem.controller;
 
+import com.example.ClinicalSystem.DTO.ClinicAdminDTO;
+import com.example.ClinicalSystem.DTO.HolidayDTO;
+import com.example.ClinicalSystem.DTO.PatientRequestDTO;
 import com.example.ClinicalSystem.service.HolidayService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -14,5 +18,16 @@ public class HolidayController {
     @Autowired
     private HolidayService holidayService;
 
-    
+    @RequestMapping(method = RequestMethod.POST, value = "/makerequest/{nurseid}")
+    @PreAuthorize("hasAuthority('NURSE')")
+    public ResponseEntity<?> makeRequest(@PathVariable String nurseid, @RequestBody HolidayDTO holidayDTO) {
+
+        boolean isApproved = holidayService.request(nurseid, holidayDTO);
+
+        if(!isApproved) {
+            return new ResponseEntity<>("Request with this start date or reason already exists!", HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>("Success!", HttpStatus.OK);
+        }
+    }
 }
