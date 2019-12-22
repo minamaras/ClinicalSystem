669f351f9c8e264f76c98f03f921f9c34c3e77b4
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2019-12-22 13:16:37 +0100

    added new predefiend clinics,admins,doctors

--- ClinicalSystem/src/main/resources/data-postgres.sql
+++ ClinicalSystem/src/main/resources/data-postgres.sql
@@ -3,17 +3,51 @@ insert into users (id,name, lastname, email, password, role,last_password_reset_
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-4,'mina', 'maras', 'mina.maras@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'DOCTOR','2019-12-01 09:00:01');
 insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-3,'tamara', 'jancic', 'tamara@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'CLINICADMIN','2019-12-01 09:00:01');
 
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-9,'biljana', 'petrovic', 'biljana@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'CLINICADMIN','2019-12-01 09:00:01');
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-10,'sandra', 'babic', 'sandra@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'CLINICADMIN','2019-12-01 09:00:01');
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-2,'olivera', 'stojanovic', 'olivera@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'CLINICADMIN','2019-12-01 09:00:01');
+
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-7,'doktor1', 'doktor1', 'doktor1@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'DOCTOR','2019-12-01 09:00:01');
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-8,'doktor2', 'doktor2', 'doktor2@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'DOCTOR','2019-12-01 09:00:01');
+insert into users (id,name, lastname, email, password, role,last_password_reset_date) values (-11,'doktor3', 'doktor3', 'doktor3@gmail.com', '$2a$10$En99NVAv.YrTtVxJ1fssBeVO4AFnfl1OMwzFbPeaDdSBm1KLUzp12', 'DOCTOR','2019-12-01 09:00:01');
+
 
 INSERT INTO clinic (id, description, name, adress,rating) values (-3, 'Klinika broj 1', 'Klinika1', 'Bulevar Oslobodjenja 22','9');
+INSERT INTO clinic (id, description, name, adress,rating) values (-5, 'Klinika broj 2', 'Klinika2', 'Njegoseva 3','6');
+INSERT INTO clinic (id, description, name, adress,rating) values (-7, 'Klinika broj 3', 'Klinika3', 'Safarikova 11','7');
+INSERT INTO clinic (id, description, name, adress,rating) values (-2, 'Klinika broj 4', 'Klinika4', 'Bulevar Evrope 8','10');
+
+insert into clinic_admin (id,clinic_id) values (-9,-5);
+insert into clinic_admin (id,clinic_id) values (-10,-7);
+insert into clinic_admin (id,clinic_id) values (-2,-2);
+
 insert into clinic_admin (id,clinic_id) values (-3,-3);
-insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id) values (-4,'zubar','10',-3,-3);
+insert into exam_type (id,name,price) values (-4,'Stomatoloski pregled',1500);
+
+insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id,exam_type_id) values (-4,'zubar','10',-3,-3,-4);
+
+insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id,exam_type_id) values (-7,'zubar','10',-5,-9,-4);
+insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id,exam_type_id) values (-8,'kardiolog','9',-7,-10,-4);
+insert into doctor (id,specialization,rating,clinic_id,clinic_admin_id,exam_type_id) values (-11,'ginekolog','7',-2,-2,-4);
+
+
 insert into clinical_centre_admin (id) values (-6);
+insert into exam_type_doctors (exam_type_id,doctors_id) values (-4,-4);
 
 insert into patient (id,active,adress,city,country,phone,socialSecurityNumber) values (-5,'true','Bulevar Oslobodjenja 11','Novi Sad','Srbija','064335512','0301997805089');
 
 insert into clinic_admin_doctors(clinic_admin_id,doctors_id) values(-3,-4);
+
+insert into clinic_admin_doctors(clinic_admin_id,doctors_id) values(-9,-7);
+insert into clinic_admin_doctors(clinic_admin_id,doctors_id) values(-10,-8);
+insert into clinic_admin_doctors(clinic_admin_id,doctors_id) values(-2,-11);
+
+
 insert into clinic_doctors (clinic_id,doctors_id) values (-3,-4);
 
+insert into clinic_doctors (clinic_id,doctors_id) values (-5,-7);
+insert into clinic_doctors (clinic_id,doctors_id) values (-7,-8);
+insert into clinic_doctors (clinic_id,doctors_id) values (-2,-11);
 
 INSERT INTO authority (id,name) values (1,'PATIENT');
 INSERT INTO authority (id,name) values (2,'DOCTOR');
