87ae9e8d90d51515637a81dcc22e2db35ce12543
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-24 03:13:41 +0100

    created method for viewing all recipes

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/RecipeController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/RecipeController.java
@@ -1,10 +1,16 @@
 package com.example.ClinicalSystem.controller;
 
+import com.example.ClinicalSystem.DTO.ClinicAdminDTO;
+import com.example.ClinicalSystem.DTO.RecipeDTO;
 import com.example.ClinicalSystem.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -15,4 +21,12 @@ public class RecipeController {
     private RecipeService recipeService;
 
 
+    @RequestMapping(method = RequestMethod.GET, value = "/all")
+    @PreAuthorize("hasAuthority('NURSE')")
+    public ResponseEntity<List<RecipeDTO>> getAllRecipes() {
+
+        List<RecipeDTO> recipes = recipeService.findAll();
+
+        return new ResponseEntity<>(recipes, HttpStatus.OK);
+    }
 }
