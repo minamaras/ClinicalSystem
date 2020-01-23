285bb18d60a1fca33cd2040be81cc5957357b7d3
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-01-23 22:01:24 +0100

    added method for finding clinic by address

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/ClinicRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/ClinicRepository.java
@@ -13,5 +13,6 @@ public interface ClinicRepository extends JpaRepository<Clinic, Long> {
 	Clinic save(Clinic clinic);
 	List<Clinic> findAll();
 	Clinic findByName(String name);
+	List<Clinic> findByAdress(String adress);
 
 }
