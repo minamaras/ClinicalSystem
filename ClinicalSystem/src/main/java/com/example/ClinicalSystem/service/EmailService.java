70fab6d8713cddfd3e237f604eff2d937279a8e8
Mina Maras <40916170+minamaras@users.noreply.github.com>
2020-01-26 11:47:57 +0100

    Implement method for sending mail

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/EmailService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/EmailService.java
@@ -25,6 +25,9 @@ public class EmailService {
     @Autowired
     private PatientService patientService;
 
+    @Autowired
+    private HolidayService holidayService;
+
     @Async
     public void sendDeclineNotificaitionAsync(User user, String explanation) throws MailException, InterruptedException {
 
@@ -34,6 +37,19 @@ public class EmailService {
         mail.setFrom(env.getProperty("spring.mail.username"));
         mail.setSubject("Clinical System: Registration");
         mail.setText("Hello " + user.getName() + ",\n\nYour request for registration on our website has been denied. Here's an explanation:\n\n" + explanation + "\n\n\nClinical System Team");
+        javaMailSender.send(mail);
+
+    }
+
+    @Async
+    public void sendRejectedHolidayAsync(User user) throws MailException, InterruptedException {
+
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(user.getEmail());
+        mail.setFrom(env.getProperty("spring.mail.username"));
+        mail.setSubject("Clinical System: Holiday/Absence Request");
+        mail.setText("Hello " + user.getName() + ",\n\nYour request for holiday/absence has been denied. Here's an explanation:\n\n" + "\n\n\nClinical System Team");
         javaMailSender.send(mail);
 
     }
