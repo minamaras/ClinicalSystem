e76da3e4a8530ff3b01e2e9e954d88515d8703d3
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-01-26 20:19:42 +0100

    created appointment request repository

---
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/AppointmentRequestController.java
@@ -0,0 +1,39 @@
+package com.example.ClinicalSystem.controller;
+
+import com.example.ClinicalSystem.DTO.AppointmentDTO;
+import com.example.ClinicalSystem.DTO.AppointmentRequestDTO;
+import com.example.ClinicalSystem.model.AppointmentRequest;
+import com.example.ClinicalSystem.service.AppointmentRequestService;
+import com.example.ClinicalSystem.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.util.List;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping(value = "api/appointmentrequest")
+public class AppointmentRequestController {
+
+    @Autowired
+    private AppointmentRequestService appointmentRequestService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/saveappointmentrequest")
+    @PreAuthorize("hasAnyAuthority('PATIENT')")
+    public ResponseEntity<?> saveAppointmentRequest(@RequestBody AppointmentRequestDTO appointmentRequestDTO) throws ParseException, UnsupportedEncodingException {
+
+        boolean issaved = appointmentRequestService.saveAppointmentRequest(appointmentRequestDTO);
+        if(issaved) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
