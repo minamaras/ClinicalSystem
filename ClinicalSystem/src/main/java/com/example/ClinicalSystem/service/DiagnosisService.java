b625c2ce287b9efc8cbe9cf2cf104ef4983faf94
tjncc <55355686+tjncc@users.noreply.github.com>
2020-01-27 22:13:55 +0100

    fixed method for diagnosis display

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/DiagnosisService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/DiagnosisService.java
@@ -1,10 +1,12 @@
 package com.example.ClinicalSystem.service;
 
+import com.example.ClinicalSystem.DTO.DiagnosisDTO;
 import com.example.ClinicalSystem.model.Diagnosis;
 import com.example.ClinicalSystem.repository.DiagnosisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,6 +30,16 @@ public class DiagnosisService {
 
     public Diagnosis findByName(String name){
         return diagnosisRepository.findByName(name);
+    }
+
+    public List<DiagnosisDTO> findAllDTOs(){
+        List<Diagnosis> diagnosis = diagnosisRepository.findAllByOrderByNameAsc();
+        List<DiagnosisDTO> diagnosisDTOS = new ArrayList<>();
+        for(Diagnosis d: diagnosis){
+            DiagnosisDTO diagnosisDTO = new DiagnosisDTO(d);
+            diagnosisDTOS.add(diagnosisDTO);
+        }
+        return diagnosisDTOS;
     }
 
 }
