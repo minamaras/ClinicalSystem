46091639c08318bbb392af381b43f80d416742b5
tjncc <55355686+tjncc@users.noreply.github.com>
2020-01-27 22:12:58 +0100

    fixed recipe verifying

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/RecipeController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/RecipeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.security.Principal;
 import java.util.List;
 
@@ -40,14 +41,19 @@ public class RecipeController {
         return new ResponseEntity<>(recipes, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/authenticate")
+    @RequestMapping(method = RequestMethod.GET, value = "/authenticate/{id}")
     @PreAuthorize("hasAuthority('NURSE')")
-    public ResponseEntity<?> authentificateRecipe(@RequestBody RecipeDTO recipeDTO, Principal p) {
+    public ResponseEntity authentificateRecipe(@PathVariable String id, Principal p) {
 
 
-        RecipeDTO recipeDTO1 = recipeService.authRecipe(recipeDTO, p);
+        boolean isAuth = recipeService.authRecipe(id, p);
 
-        return new ResponseEntity<>(recipeDTO1, HttpStatus.OK);
+        if(isAuth) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/patients")
