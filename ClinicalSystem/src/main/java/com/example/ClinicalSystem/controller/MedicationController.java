9cf4023bce9ce66a3b2b9d64ebdb7ad88ab6ea98
tjncc <55355686+tjncc@users.noreply.github.com>
2020-01-27 14:45:24 +0100

    created method for getting all medications names

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/MedicationController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/MedicationController.java
@@ -1,12 +1,17 @@
 package com.example.ClinicalSystem.controller;
 
+import com.example.ClinicalSystem.DTO.DiagnosisNamesDTO;
+import com.example.ClinicalSystem.DTO.MedicationNamesDTO;
+import com.example.ClinicalSystem.model.Diagnosis;
 import com.example.ClinicalSystem.model.Medication;
 import com.example.ClinicalSystem.service.MedicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
@@ -35,5 +40,21 @@ public class MedicationController {
         List<Medication> medications = medicationService.findAll();
 
         return new ResponseEntity<>(medications, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/allmedicationsnames")
+    @PreAuthorize("hasAnyAuthority('DOCTOR','NURSE')")
+    public ResponseEntity<List<MedicationNamesDTO>> getAllMedicationNames() {
+
+        List<Medication> medications = medicationService.findAll();
+        List<MedicationNamesDTO> medicationNamesDTOS = new ArrayList<>();
+
+        for(Medication med : medications){
+            MedicationNamesDTO mDTO = new MedicationNamesDTO(med.getName());
+            medicationNamesDTOS.add(mDTO);
+        }
+
+
+        return new ResponseEntity<>(medicationNamesDTOS, HttpStatus.OK);
     }
 }
