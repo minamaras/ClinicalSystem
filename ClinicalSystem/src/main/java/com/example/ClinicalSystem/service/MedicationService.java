c8d0922a53a74f656350572ef77306c77eb23eec
tjncc <55355686+tjncc@users.noreply.github.com>
2020-01-27 22:14:13 +0100

    fixed method for medication display

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/MedicationService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/MedicationService.java
@@ -1,10 +1,12 @@
 package com.example.ClinicalSystem.service;
 
+import com.example.ClinicalSystem.DTO.MedicationDTO;
 import com.example.ClinicalSystem.model.Medication;
 import com.example.ClinicalSystem.repository.MedicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,10 +25,22 @@ public class MedicationService {
     }
 
     public List<Medication> findAll(){
-        return medicationRepository.findAll();
+        return medicationRepository.findAllByOrderByNameAsc();
     }
 
     public Medication findByName(String name){
         return medicationRepository.findByName(name);
+    }
+
+    public List<MedicationDTO> findAllDTOs(){
+        List<Medication> medications = medicationRepository.findAllByOrderByNameAsc();
+        List<MedicationDTO> medicationDTOS = new ArrayList<>();
+
+        for(Medication med: medications){
+            MedicationDTO medicationDTO = new MedicationDTO(med);
+            medicationDTOS.add(medicationDTO);
+        }
+
+        return medicationDTOS;
     }
 }
