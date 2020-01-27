89f4d980f28615b7ceb884724b7039eb1c8cff38
tjncc <55355686+tjncc@users.noreply.github.com>
2020-01-27 14:54:17 +0100

    fixed recipe model

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/model/Recipe.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/model/Recipe.java
@@ -2,15 +2,9 @@ package com.example.ClinicalSystem.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -25,31 +19,34 @@ public class Recipe {
 
 	@JsonIgnore
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Patient patient;
+
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private Nurse nurse;
 	
 	@Column(name = "isAuthenthicated")
 	private boolean auth;
 
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private Patient patient;
-	
-	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "recipe_medication", joinColumns = @JoinColumn(name = "recipe_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "medication_id", referencedColumnName = "id"))
+	private Set<Medication> medications = new HashSet<Medication>();
 	
 	@Column(name = "content")
 	private String content;
 
 	public Recipe() {
-		
+		this.auth = false;
 	}
 	
-	public Recipe(Long id, Doctor doctor, Nurse nurse, Patient patient, String content) {
+	public Recipe(Long id, Doctor doctor, Nurse nurse, String content, Patient patient) {
 		super();
 		this.id = id;
 		this.doctor = doctor;
 		this.nurse = nurse;
-		this.patient = patient;
 		this.content = content;
+		this.patient = patient;
+		this.auth = false;
 	}
 
 	public Long getId() {
@@ -76,14 +73,6 @@ public class Recipe {
 		this.nurse = nurse;
 	}
 
-	public Patient getPatient() {
-		return patient;
-	}
-
-	public void setPatient(Patient patient) {
-		this.patient = patient;
-	}
-
 	public String getContent() {
 		return content;
 	}
@@ -98,5 +87,21 @@ public class Recipe {
 
 	public void setAuth(boolean auth) {
 		this.auth = auth;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Set<Medication> getMedications() {
+		return medications;
+	}
+
+	public void setMedications(Set<Medication> medications) {
+		this.medications = medications;
 	}
 }
