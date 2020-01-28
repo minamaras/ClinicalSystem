42deffdae778e9d9724ec40cb4aa6653cb231482
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-01-28 14:59:24 +0100

    added method for searching by date

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
@@ -6,11 +6,13 @@ import com.example.ClinicalSystem.model.ExamType;
 import com.example.ClinicalSystem.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
 public interface AppointmentRequestRepository extends JpaRepository<AppointmentRequest, Long> {
 
     AppointmentRequest save(AppointmentRequest a);
     Long removeById(Long id);
     AppointmentRequest findByName(String name);
     AppointmentRequest findByPatient(Patient p);
+    AppointmentRequest findByStart(Date start);
     AppointmentRequest findByType(ExamType type);
 }
