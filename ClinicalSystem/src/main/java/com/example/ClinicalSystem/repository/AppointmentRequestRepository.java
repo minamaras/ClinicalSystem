e36deebd83d3401edf3b41ecf10b87c961de5111
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-01-28 14:58:58 +0100

    added method for searching by examtype

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
@@ -2,6 +2,7 @@ package com.example.ClinicalSystem.repository;
 
 import com.example.ClinicalSystem.model.Appointment;
 import com.example.ClinicalSystem.model.AppointmentRequest;
+import com.example.ClinicalSystem.model.ExamType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRequestRepository extends JpaRepository<AppointmentRequest, Long> {
@@ -9,4 +10,5 @@ public interface AppointmentRequestRepository extends JpaRepository<AppointmentR
     AppointmentRequest save(AppointmentRequest a);
     Long removeById(Long id);
     AppointmentRequest findByName(String name);
+    AppointmentRequest findByType(ExamType type);
 }
