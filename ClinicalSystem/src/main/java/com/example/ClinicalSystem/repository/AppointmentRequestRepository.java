3660f43c1ac14db77d982f807b2e518515fee27c
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-01-28 14:59:13 +0100

    added method for searching by patient

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
@@ -3,6 +3,7 @@ package com.example.ClinicalSystem.repository;
 import com.example.ClinicalSystem.model.Appointment;
 import com.example.ClinicalSystem.model.AppointmentRequest;
 import com.example.ClinicalSystem.model.ExamType;
+import com.example.ClinicalSystem.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRequestRepository extends JpaRepository<AppointmentRequest, Long> {
@@ -10,5 +11,6 @@ public interface AppointmentRequestRepository extends JpaRepository<AppointmentR
     AppointmentRequest save(AppointmentRequest a);
     Long removeById(Long id);
     AppointmentRequest findByName(String name);
+    AppointmentRequest findByPatient(Patient p);
     AppointmentRequest findByType(ExamType type);
 }
