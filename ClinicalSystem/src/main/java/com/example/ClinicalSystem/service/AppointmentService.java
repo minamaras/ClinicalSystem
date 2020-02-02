ffb600dd5b72237e44c6799bc0e4648632b41de8
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-02-02 17:28:02 +0100

    created methods for getting all previous and all upcoming exams

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/AppointmentService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/AppointmentService.java
@@ -1,21 +1,22 @@
 package com.example.ClinicalSystem.service;
 
-import com.example.ClinicalSystem.DTO.AppointmentDTO;
-import com.example.ClinicalSystem.DTO.NurseDTO;
-import com.example.ClinicalSystem.DTO.OperationRoomDTO;
+import com.example.ClinicalSystem.DTO.*;
 import com.example.ClinicalSystem.model.*;
 import com.example.ClinicalSystem.repository.AppointmentRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
 import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class AppointmentService {
@@ -30,10 +31,42 @@ public class AppointmentService {
     private ExamTypeService examTypeService;
 
     @Autowired
+    private PatientService patientService;
+
+    @Autowired
     private OperationRoomService operationRoomService;
 
     @Autowired
     private ModelMapper modelMapper;
+
+    public boolean saveAppointment(AppointmentDTO appointmentDTO){
+
+        //fsdgsdgs
+
+        if(appointmentRepository.findById(appointmentDTO.getId()) == null){
+            return  false;
+        }else {
+
+            Optional<Appointment> appointmentop = appointmentRepository.findById(appointmentDTO.getId());
+            Appointment appointment = appointmentop.get();
+
+            Authentication a = SecurityContextHolder.getContext().getAuthentication();
+            User user = (User) a.getPrincipal();
+
+            if(user.getRole() == Role.PATIENT) {
+                Patient loggedinpatient = patientService.findPatient(user.getEmail());
+                appointment.setPatient(loggedinpatient);
+            }
+
+            appointment.setClassification(AppointmentClassification.NORMAL);
+            appointment.setStatus(AppointmentStatus.SHEDULED);
+
+
+            appointmentRepository.save(appointment);
+            return true;
+    }
+
+    }
 
     public List<AppointmentDTO> findAllPredefined() {
 
@@ -69,10 +102,62 @@ public class AppointmentService {
         appointment.setStart(finaldate);
 
         appointment.setClassification(AppointmentClassification.PREDEFINED);
-        appointment.setStatus(AppointmentStatus.AVALIABLE);
+        appointment.setStatus(AppointmentStatus.SHEDULED);
 
         appointmentRepository.save(appointment);
 
         return true;
     }
+
+    public Set<AppointmentDTO> getAllExams(){
+
+        Authentication a = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) a.getPrincipal();
+        Set<AppointmentDTO> returnAppointments = new HashSet<>();
+
+        if(user.getRole() == Role.PATIENT) {
+            Patient loggedinpatient = patientService.findPatient(user.getEmail());
+            Set<Appointment> appointments = loggedinpatient.getAppointments();
+
+
+            for (Appointment ap : appointments){
+                if(ap.getStatus() ==  AppointmentStatus.SHEDULED && ap.getClassification() == AppointmentClassification.NORMAL){
+
+                    AppointmentDTO appointmentDTO = modelMapper.map(ap,AppointmentDTO.class);
+                    appointmentDTO.setDate(ap.getStart().toString().substring(0,10));
+                    returnAppointments.add(appointmentDTO);
+
+                }
+
+            }
+        }
+        return returnAppointments;
+    }
+
+
+    public Set<AppointmentDTO> getAllExamsOld(){
+
+        Authentication a = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) a.getPrincipal();
+        Set<AppointmentDTO> returnAppointments = new HashSet<>();
+
+        if(user.getRole() == Role.PATIENT) {
+            Patient loggedinpatient = patientService.findPatient(user.getEmail());
+            Set<Appointment> appointments = loggedinpatient.getAppointments();
+
+
+            for (Appointment ap : appointments){
+                if(ap.getStatus() ==  AppointmentStatus.HAS_HAPPEND && ap.getClassification() == AppointmentClassification.NORMAL){
+
+                    AppointmentDTO appointmentDTO = modelMapper.map(ap,AppointmentDTO.class);
+                    appointmentDTO.setDate(ap.getStart().toString().substring(0,10));
+                    returnAppointments.add(appointmentDTO);
+
+                }
+
+            }
+        }
+        return returnAppointments;
+    }
+
 }
