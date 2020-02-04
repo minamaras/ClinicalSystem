f7f00527a489920a5ef961c07a88674a95327b96
tjncc <55355686+tjncc@users.noreply.github.com>
2020-02-05 00:40:36 +0100

    created model for operation requests

---
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/model/OperationRequest.java
@@ -0,0 +1,95 @@
+package com.example.ClinicalSystem.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class OperationRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "startdate" , nullable = false)
+    private Date start;
+
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    @Column(name = "starttime", nullable = false )
+    private Time startTime;
+
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    @Column(name = "endtime", nullable = false )
+    private Time endTime;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Patient patient;
+
+    public OperationRequest() {
+        super();
+    }
+
+    public OperationRequest(long id, String name, Date start, Time startTime, Time endTime, Patient patient){
+        this.id = id;
+        this.name = name;
+        this.start = start;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.patient = patient;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+}
