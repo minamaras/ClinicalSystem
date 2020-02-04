a574f94920df539c508ad257c39b19e79c4f5926
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-02-04 12:50:53 +0100

    Update AppointmentRepository.java

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRepository.java
@@ -1,7 +1,10 @@
 package com.example.ClinicalSystem.repository;
 
 import com.example.ClinicalSystem.model.Appointment;
+import com.example.ClinicalSystem.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
