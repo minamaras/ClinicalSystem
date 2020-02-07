93b3e7e04f499bc7c143c19ad19f293679868218
tjncc <55355686+tjncc@users.noreply.github.com>
2020-02-07 18:31:21 +0100

    implemented automatically scheduling operations

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/OperationRequestService.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/service/OperationRequestService.java
@@ -1,18 +1,21 @@
 package com.example.ClinicalSystem.service;
 
 import com.example.ClinicalSystem.DTO.DoctorDTO;
+import com.example.ClinicalSystem.DTO.OperationParamsDTO;
 import com.example.ClinicalSystem.DTO.OperationRequestDTO;
+import com.example.ClinicalSystem.DTO.OperationRoomDTO;
 import com.example.ClinicalSystem.model.*;
 import com.example.ClinicalSystem.repository.OperationRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.util.*;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class OperationRequestService {
@@ -74,14 +77,23 @@ public class OperationRequestService {
             return false;
         }
 
+        if(operation.isScheduled()){
+            return true;
+        }
+
         java.util.Date originalDate = operation.getStart();
 
-        String inputDateString = operationRequestDTO.getStart();
+        String inputDateString;
+        if(operationRequestDTO.getStart() == null) {
+            inputDateString = operationRequestDTO.getDate();
+        } else {
+            inputDateString = operationRequestDTO.getStart();
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
         LocalDate inputDate = LocalDate.parse(inputDateString);
         java.sql.Date finalDate = java.sql.Date.valueOf(inputDate);
 
-        LocalTime startTime = operationRequestDTO.getStartTime().toLocalTime();
+        LocalTime startTime = operation.getStartTime().toLocalTime();
         LocalTime endTime = startTime.plusHours(2);
         Time finalStartTime = Time.valueOf(startTime);
         Time finalEndTime = Time.valueOf(endTime);
@@ -99,15 +111,18 @@ public class OperationRequestService {
         operation.setOr(oroom);
         //save(operation);
 
+
         oroom.getOperations().add(operation);
 
 
 
         for(String name : operationRequestDTO.getDoctorNames()){
             Doctor doctor = doctorService.findOne(name);
-            operation.getDoctors().add(doctor);
-            emailService.sendOperationInfoDoctor(doctor,thisD,starts);
-            //doctor.getOperations().add(operation);
+          //  if(!operation.getDoctors().contains(doctor)) {
+                operation.getDoctors().add(doctor);
+                emailService.sendOperationInfoDoctor(doctor, thisD, starts);
+                //doctor.getOperations().add(operation);
+          //  }
         }
 
         operation.setScheduled(true);
@@ -127,6 +142,67 @@ public class OperationRequestService {
 
 
         return true;
+
+    }
+
+
+
+    public void assignOperationAutomatically() throws ParseException, InterruptedException {
+        List<OperationRequest> operations = operationRequestRepository.findAll();
+
+        List<OperationRequest> notChecked = new ArrayList<>();
+        for (OperationRequest op : operations){
+            if(!op.isScheduled()){
+                notChecked.add(op);
+            }
+        }
+
+        if(!notChecked.isEmpty()) {
+
+            for (OperationRequest operation : notChecked) {
+
+                if (operation.isScheduled()) {
+                    return;
+                }
+
+                Random random = new Random();
+                //int minDay = (int) new Date().getTime();
+                int minDay = (int) LocalDate.of(2020, 1, 1).toEpochDay();
+                int maxDay = (int) LocalDate.of(2021, 1, 1).toEpochDay();
+                long randomDay = minDay + random.nextInt(maxDay - minDay);
+
+                LocalDate randomDate = LocalDate.ofEpochDay(randomDay);
+                java.util.Date finalDate = java.sql.Date.valueOf(randomDate);
+                String thisD = finalDate.toString().substring(0, 10);
+
+
+                OperationRequestDTO operationRequestDTO = new OperationRequestDTO();
+                operationRequestDTO.setId(operation.getId());
+                operationRequestDTO.setDate(thisD);
+                operationRequestDTO.setStartTime(operation.getStartTime());
+                operationRequestDTO.setEndTime(operation.getEndTime());
+
+                List<OperationRoomDTO> operationRoomDTOS = operationRoomService.allRoomsForOperationRandom(finalDate, operation.getStartTime(), operation.getEndTime());
+
+                int randomRoom = (int) (Math.random() * operationRoomDTOS.size());
+                int roomNum = operationRoomDTOS.get(randomRoom).getNumber();
+                OperationParamsDTO operationParamsDTO = new OperationParamsDTO(operation.getId(), thisD, operation.getStartTime().toString(), roomNum);
+
+                List<DoctorDTO> freeDoctors = doctorService.getFreeDoctorsForOperation(operationParamsDTO);
+
+                operationRequestDTO.setRoomNumber(roomNum);
+
+                List<String> docNames = new ArrayList<>();
+                for (DoctorDTO doc : freeDoctors) {
+                    docNames.add(doc.getEmail());
+                }
+                operationRequestDTO.setDoctorNames(docNames);
+
+                scheduleOperation(operationRequestDTO);
+
+
+            }
+        }
 
     }
 
