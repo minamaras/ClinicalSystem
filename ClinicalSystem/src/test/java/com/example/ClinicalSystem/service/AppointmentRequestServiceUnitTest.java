5d1815d1514740a2621ac18839d811c552e2803c
Mina Maras <40916170+minamaras@users.noreply.github.com>
2020-02-09 11:41:02 +0100

    Started isCreated unit test

--- ClinicalSystem/src/test/java/com/example/ClinicalSystem/service/AppointmentRequestServiceUnitTest.java
+++ ClinicalSystem/src/test/java/com/example/ClinicalSystem/service/AppointmentRequestServiceUnitTest.java
@@ -6,13 +6,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
@@ -20,10 +24,13 @@ import static org.mockito.BDDMockito.given;
 public class AppointmentRequestServiceUnitTest {
 
     @Autowired
-    AppointmentRequestService appointmentRequestService;
+    private AppointmentRequestService appointmentRequestService;
 
     @MockBean
-    AppointmentRequestRepository appointmentRequestRepository;
+    private AppointmentRequestRepository appointmentRequestRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Before
     public void setup() {
@@ -32,7 +39,7 @@ public class AppointmentRequestServiceUnitTest {
         patient1.setLastname("Maras");
         patient1.setEmail("mina@gmail.com");
 
-        ExamType examType1 = new ExamType("Ocni pregled", 1500, 30);
+        ExamType examType1 = new ExamType("Stomatoloski pregled", 1500, 30);
 
         OR or1 = new OR(3,"Soba", examType1);
 
@@ -42,15 +49,43 @@ public class AppointmentRequestServiceUnitTest {
         doctor1.setEmail("tamaraa@gmail.com");
         doctor1.setExamType(examType1);
         doctor1.setSpecialization("Zubar");
+        Time stime1 = Time.valueOf("13:00:00");
+        Time etime1 = Time.valueOf("20:00:00");
+        doctor1.setStart(stime1);
+        doctor1.setEnd(etime1);
+
+        Doctor doctor2 = new Doctor();
+        doctor2.setName("Jelena");
+        doctor2.setLastname("Bojanic");
+        doctor2.setEmail("jelenaa@gmail.com");
+        doctor2.setExamType(examType1);
+        doctor2.setSpecialization("Stomatolog");
+        Time stime2 = Time.valueOf("10:00:00");
+        Time etime2 = Time.valueOf("20:00:00");
+        doctor2.setStart(stime2);
+        doctor2.setEnd(etime2);
+
+        Date examDate = Date.valueOf("2020-03-05");
+        Time examStart = Time.valueOf("14:00:00");
+        Time examEnd = Time.valueOf("14:15:00");
 
         given(appointmentRequestRepository.findByName("Stomatoloski pregled")
-        ).willReturn(new AppointmentRequest("Stomatoloski pregled", examType1, or1.getId(), patient1, doctor1));
+        ).willReturn(new AppointmentRequest(1L,"Stomatoloski pregled", examType1, or1.getId(), patient1, doctor1, examDate, examStart, examEnd));
+
+        given(appointmentRequestService.findByIdModel(1L)
+        ).willReturn(Optional.of(new AppointmentRequest(1L, "Stomatoloski pregled", examType1, or1.getId(), patient1, doctor1, examDate, examStart, examEnd)));
     }
 
     @Test
     public void testFindByNameAppointmentRequest() {
         AppointmentRequest result = appointmentRequestService.findByName("Stomatoloski pregled");
         Assert.assertEquals("Stomatoloski pregled", result.getName());
+    }
+
+    @Test
+    public void testIsCreated() {
+        Optional<AppointmentRequest> apreq = appointmentRequestService.findByIdModel(1L);
+        assertThat(apreq).isNotNull();
     }
 
 }
