5188f829c48f59a7472908f116c5a3b91a1364b2
tjncc <55355686+tjncc@users.noreply.github.com>
2020-02-09 19:30:22 +0100

    Update AppointmentController.java

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/AppointmentController.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/controller/AppointmentController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.Principal;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Set;
