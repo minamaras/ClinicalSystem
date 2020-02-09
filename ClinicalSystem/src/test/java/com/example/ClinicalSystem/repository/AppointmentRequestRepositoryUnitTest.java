58d5a6a64d4057965f629c71f07aeca751efe27b
Mina Maras <40916170+minamaras@users.noreply.github.com>
2020-02-09 11:41:12 +0100

    Removed comments

--- ClinicalSystem/src/test/java/com/example/ClinicalSystem/repository/AppointmentRequestRepositoryUnitTest.java
+++ ClinicalSystem/src/test/java/com/example/ClinicalSystem/repository/AppointmentRequestRepositoryUnitTest.java
@@ -27,57 +27,6 @@ public class AppointmentRequestRepositoryUnitTest {
 
     @Before
     public void setUp() {
-       /*User userPatient1 = new User(6L, Role.PATIENT,"Mina", "Maras", "minamarass@gmail.com");
-        User userPatient2 = new User(1L, Role.PATIENT,"Petar", "Petrovic", "petrovic@gmail.com");
-
-        entityManager.merge(userPatient1);
-        Patient patient1 = new Patient();
-        patient1.setName(userPatient1.getName());
-        patient1.setLastname(userPatient1.getLastname());
-        patient1.setEmail(userPatient1.getEmail());
-
-
-
-        User userDoctor1 = new User(2L, Role.DOCTOR, "Tamara", "Jancic", "tamarajancic@gmail.com");
-        User userDoctor2 = new User(3L, Role.DOCTOR, "Jelena", "Bojanic", "jelenabojanic@gmail.com");
-        User userDoctor3 = new User(4L, Role.DOCTOR, "Tijana", "Maras", "tijanamaras@gmail.com");
-
-        ExamType examType1 = new ExamType("Ocni pregled", 1500, 30);
-        entityManager.merge(examType1);
-
-        entityManager.merge(userDoctor1);
-        Doctor doctor1 = new Doctor();
-        doctor1.setName(userDoctor1.getName());
-        doctor1.setLastname(userDoctor1.getLastname());
-        doctor1.setEmail(userDoctor1.getEmail());
-        doctor1.setExamType(examType1);
-
-
-
-        User userClinicAdmin1 = new User(5L, Role.CLINICADMIN, "Petar", "Stupar", "petarstrupar@gmail.com");
-
-        //entityManager.persist(userPatient1);
-        entityManager.merge(userPatient2);
-        entityManager.merge(userDoctor1);
-        entityManager.merge(userDoctor2);
-        entityManager.merge(userDoctor3);
-        entityManager.merge(userClinicAdmin1);
-
-        Clinic clinic1 = new Clinic("Klinika Novi Sad", "Brace Ribnikar 29", "hahah");
-
-        entityManager.merge(clinic1);
-
-
-        OR or1 = new OR(3,"Soba", examType1);
-        entityManager.merge(or1);
-
-
-
-        entityManager.merge(patient1);
-
-
-
-        entityManager.merge(doctor1);*/
 
         Patient patient1 = new Patient();
         patient1.setName("Mina");
