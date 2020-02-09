fde7bbdf610f82b782ff85c7b208c64a617a1720
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-02-09 19:47:37 +0100

    added lock

--- ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
+++ ClinicalSystem/src/main/java/com/example/ClinicalSystem/repository/AppointmentRequestRepository.java
@@ -4,13 +4,23 @@ import com.example.ClinicalSystem.model.AppointmentRequest;
 import com.example.ClinicalSystem.model.ExamType;
 import com.example.ClinicalSystem.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.sql.Date;
 import java.sql.Time;
 import java.util.List;
 import java.util.Optional;
 
 public interface AppointmentRequestRepository extends JpaRepository<AppointmentRequest, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from AppointmentRequest a")
+    @QueryHints({ @QueryHint(name = "javax.persistence.lock.timeout", value = "0") })
+    List<AppointmentRequest> findAll();
 
     AppointmentRequest save(AppointmentRequest a);
     Long removeById(Long id);
